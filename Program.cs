5018135b8a943e3a7479c0a527ea60d481d9e0d9
Сергей Галкин <sergeygalckin2012@yandex.ru>
2021-05-28 14:04:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a;            
+            Console.WriteLine("введите число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("четное число");
+
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+
+            if (step(a)) Console.WriteLine("yes");
+            else Console.WriteLine("No");          
+
+            Console.ReadKey();
+        }
+
+        public static bool step(int a)
+        {
+            if (a == 2) return true;
+            else if (a % 2 == 0) return step(a / 2);
+            else return false;
+        }
+    }
+}
